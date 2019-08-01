7d6d79f7c46fa675482756b3cf9453317502b566
fangzhancai <1119405009@qq.com>
2019-08-01 10:49:14 +0800

    提交

---
+++ 初识Python/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   __init__.py.py    
+@Contact :   1119405009@qq.com
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2019/8/1 10:37   felix      1.0         None
+'''
+# import lib
+
+print('hello, world!')
+# print("你好,世界！")
+print('你好', '世界')
+print('hello', 'world', sep=', ', end='!')
+print("嗯", sep=', ', end='!')
+print('goodbye, world', end='!\n')
+
